b62d9a7e8e01eff9f08a225d6ecca68b0f7e37a9
kusocisme <114485513+kusocisme@users.noreply.github.com>
2023-03-31 17:33:07 +0700

    Week04
    hehe

---
+++ src/main/java/murach/email/EmailListServlet.java
@@ -0,0 +1,58 @@
+package murach.email;
+
+import murach.business.User;
+import murach.data.UserDB;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "EmailListServlet", value = "/emailList")
+public class EmailListServlet extends HttpServlet
+{    
+    @Override
+    protected void doPost(HttpServletRequest request, 
+                          HttpServletResponse response) 
+                          throws ServletException, IOException {
+        String url = "/index.html";
+        
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "join";
+        }
+        
+        if (action.equals("add")) {                
+            // get parameters from the request
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String email = request.getParameter("email");
+
+            // use regular Java classes
+            User user = new User(firstName, lastName, email);
+            UserDB.insert(user);
+
+            // store the User object in request and set URL
+            request.setAttribute("user", user);
+            url = "/thanks.jsp";
+        }
+        else if (action.equals("join")) {
+            // set URL to index page
+            url = "/index.html";            
+        }
+        
+        // forward request and response objects
+        getServletContext()
+            .getRequestDispatcher(url)
+            .forward(request, response);
+    }    
+    
+    @Override
+    protected void doGet(HttpServletRequest request, 
+                          HttpServletResponse response) 
+                          throws ServletException, IOException {
+        doPost(request, response);
+    }    
+}
